a0fd88329b7e8c2fd806f5bee6b186e95badd0e1
SANJAY <sanjaypk986@gmail.com>
2024-07-03 12:55:13 +0530

    implemented delete function

--- src/components/todoCard/todoCard.jsx
+++ src/components/todoCard/todoCard.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-const TodoCard = ({data}) => {
+const TodoCard = ({ data,index,todo,setTodo }) => {
+    function handleDelete(){
+        console.log("clicked");
+        setTodo(todo.filter((item, idx) => idx !== index));
+    }
   return (
-    <div>
-        <span>{data}</span>
+    <div className='flex flex-col justify-between h-full bg-white shadow-lg rounded-lg p-4 border border-gray-200 '>
+      <span className='text-lg sm:text-xl md:text-2xl text-black break-words'>
+        {data}
+      </span>
+      <div className='flex justify-end items-center mt-2'>
+        <i className='fa-solid fa-trash cursor-pointer hover:text-red-500 mr-4' onClick={handleDelete}></i>
+        <i className='fa-solid fa-pen-to-square cursor-pointer hover:text-blue-500'></i>
+      </div>
     </div>
-  )
+  );
 }
 
-export default TodoCard
+export default TodoCard;
--- src/pages/todoList.jsx
+++ src/pages/todoList.jsx
@@ -1,29 +1,28 @@
-import React, { useState } from 'react';
-import TodoForm from '../components/todoForm/todoForm';
-import TodoCard from '../components/todoCard/todoCard';
+import React, { useState } from "react";
+import TodoForm from "../components/todoForm/todoForm";
+import TodoCard from "../components/todoCard/todoCard";
 
 const TodoList = () => {
-    const [todo,setTodo] = useState([])
+  const [todo, setTodo] = useState([]);
   return (
     <main>
-        <section className='container mx-auto my-4'>
-            <h2 className='text-xl sm:text-2xl md:4xl text-center py-5 font-semibold'>Todo List</h2>
-            <div className='bg-gray-100'>
-            <TodoForm setTodo = {setTodo} />
-            </div>
-            <ul>
-                {
-                    todo && todo.map((data,index)=>(
-                        <li key={index} >
-                            <TodoCard data={data} />
-                        </li>
-                    ))
-                }
-            </ul>
+      <section className="container mx-auto my-4">
+        <h2 className="text-xl sm:text-2xl md:4xl text-center py-5 font-semibold">
+          Todo List
+        </h2>
+        <div className="bg-gray-100">
+          <TodoForm setTodo={setTodo} />
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+        {todo &&
+          todo.map((data, index) => (
+              <TodoCard key={index} index={index} data={data} todo={todo} setTodo={setTodo} />
 
-        </section>
+          ))}
+        </div>
+      </section>
     </main>
   );
-}
+};
 
 export default TodoList;
