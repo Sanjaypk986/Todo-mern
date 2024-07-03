6810ba9b21a834bf2add9cd1c1e8a88ceb3c890d
SANJAY <sanjaypk986@gmail.com>
2024-07-03 15:42:39 +0530

    Todo stores in database

--- src/components/todoCard/todoCard.jsx
+++ src/components/todoCard/todoCard.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const TodoCard = ({ data,index,todo,setTodo }) => {
     function handleDelete(){
-        console.log("clicked");
+
         setTodo(todo.filter((item, idx) => idx !== index));
+    }
+    function handleEdit (){
+        console.log("clicked");
     }
   return (
     <div className='flex flex-col justify-between h-full bg-white shadow-lg rounded-lg p-4 border border-gray-200 '>
@@ -12,7 +15,7 @@ const TodoCard = ({ data,index,todo,setTodo }) => {
       </span>
       <div className='flex justify-end items-center mt-2'>
         <i className='fa-solid fa-trash cursor-pointer hover:text-red-500 mr-4' onClick={handleDelete}></i>
-        <i className='fa-solid fa-pen-to-square cursor-pointer hover:text-blue-500'></i>
+        <i className='fa-solid fa-pen-to-square cursor-pointer hover:text-blue-500' onClick={handleEdit}></i>
       </div>
     </div>
   );
--- src/components/todoForm/todoForm.jsx
+++ src/components/todoForm/todoForm.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import { useForm } from "react-hook-form";
-import './todoForm.css'
+import axios from "axios";
 
-export default function TodoForm({setTodo}) {
+export default function TodoForm({ setTodo }) {
   
   const {
     register,
@@ -10,24 +10,33 @@ export default function TodoForm({setTodo}) {
     formState: { errors },
     reset
   } = useForm();
-  const onSubmit = (data) => {
-    setTodo((previousTodo) => [
-      ...previousTodo, 
-      data.todo
-    ]);
-    reset(); 
+
+  const onSubmit = async (data) => {
+    try {
+      // call add todos url and set state.
+      await axios.post(`${import.meta.env.VITE_BASE_URL}/todos`, data);
+      setTodo((previousTodo) => [
+        ...previousTodo,
+        data.todo
+      ]);
+      reset(); // Reset the form fields after successful submission
+      console.log(data);
+    } catch (error) {
+      console.error("Error adding todo:", error);
+    }
   };
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="flex  flex-col justify-around items-center w-full p-3 gap-4"
+      className="flex flex-col justify-around items-center w-full p-3 gap-4"
     >
       <input
-        {...register("todo", { required: true })} placeholder="Add your todos..."
-        className="w-3/4 text-xl shadow-sm px-4 py-2 border-2 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent "
+        {...register("todo", { required: true })}
+        placeholder="Add your todos..."
+        className="w-3/4 text-xl shadow-sm px-4 py-2 border-2 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent"
       />
-      {errors.todo && <span>This field is required</span>}
+      {errors.todo && <span className="text-red-500">This field is required</span>}
       <input type="submit" className="px-5 py-2 submit-button rounded-md" />
     </form>
   );
